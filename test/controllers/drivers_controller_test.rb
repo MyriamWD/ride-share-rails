540bfccd5f069536771cdd359b7a7996296fdb5d
MyriamWD <myriam.waldend@gmail.com>
2019-04-17 13:48:19 -0700

    added partial form for update and new driver. created test for inde and show in drivers_controller

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -3,19 +3,33 @@ require "test_helper"
 describe DriversController do
   describe "index" do
     it "can get index" do
-      # Your code here
-      get driver_path
+      get drivers_path
 
       must_respond_with :success
     end
   end
 
   describe "show" do
-    # Your tests go here
+    it "should be ok to show an existing, valid driver" do
+      # skip
+      driver = Driver.create(name: "test driver", vin: "badnvkadkvbdjoihuy79y")
+      valid_driver_id = driver.id
+
+      get driver_path(valid_driver_id)
+
+      must_respond_with :success
+    end
+
+    it "should give a 404 instead of showing a non-existant, invalid driver" do
+      invalid_driver_id = 8989898989
+
+      get driver_path(invalid_driver_id)
+      #determine how we want to handle this situation... probably redirect
+      must_respond_with :not_found
+    end
   end
 
   describe "edit" do
-    # Your tests go here
   end
 
   describe "update" do
