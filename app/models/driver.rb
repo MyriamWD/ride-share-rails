d63f7691df277be836c8afa437c7bdf0d731a6e9
MyriamWD <myriam.waldend@gmail.com>
2019-04-17 19:43:31 -0700

    calculated  average rating and total earnings of driver and displayed them in the show view for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,9 +5,13 @@ class Driver < ApplicationRecord
   validates :vin, presence: true
 
   def average_rating
-    average = Trip.average(:rating).where(driver_id: driver_id)
+    average = self.trips.average(:rating).round
+    return average.round(2)
   end
 
-  def total_earnigns
+  def total_earnings
+    total = self.trips.sum { |trip| (trip.cost - 165.0) * 0.80 } / 100
+
+    return total.round(2)
   end
 end
