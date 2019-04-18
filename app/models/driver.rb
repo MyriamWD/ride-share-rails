2b5a9486d4d0dc0df81b6a926f45c96f0da2de18
MyriamWD <myriam.waldend@gmail.com>
2019-04-17 18:00:46 -0700

    fixed the display of cost in driver show view. Added method to calculate average rating (not finished yet)

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,11 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
+
+  def average_rating
+    Trip.average(:rating)
+  end
+
+  def total_earnigns
+  end
 end
