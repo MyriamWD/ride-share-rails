6a42a204586cae9c4b03de001b386eccb3c0f218
MyriamWD <myriam.waldend@gmail.com>
2019-04-18 16:25:32 -0700

    added test for trips show

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,38 @@
 class TripsController < ApplicationController
+  #   def index
+  #     # We migth not need this index action - we can decide if we want to display information about it
+  #     @trips = Trips.all.order(:id)
+  #   end
+
+  def show
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
+    if @trip.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @trip = Trip.new(:date, :driver_id, :passenger_id, :cost, :rating)
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    is_successful = @trip.save
+
+    if is_successful
+      redirect_to trip_path(@trip.id)
+    else
+      render :new, status: :bad_request
+    end
+  end
+
   def destroy
     trip = Trip.find_by(id: params[:id])
     trip.destroy
+  end
+
+  def trip_params
+    return params.require(:trip).permit(:date, :driver_id, :passenger_id, :cost, :rating)
   end
 end
--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,7 +2,11 @@ require "test_helper"
 
 describe TripsController do
   describe "show" do
-    # Your tests go here
+    it "should show an existing, valid trip" do
+      trip = Trip.create(date: "2019-4-18", driver_id: Driver.create(name: "Myriam", vin: "vinnumber").id, passenger_id: Passenger.create(name: "NotMyriam", phone_num: "phone humber call").id, rating: 5, cost: 3890)
+      get trip_path(trip.id)
+      must_respond_with :success
+    end
   end
 
   describe "edit" do
@@ -15,6 +19,7 @@ describe TripsController do
 
   describe "create" do
     # Your tests go here
+
   end
 
   describe "destroy" do
