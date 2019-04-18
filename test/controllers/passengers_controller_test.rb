2ea04b045dae9332c0134af5a2fd3135c191e48c
Ngoc Le <ngocle@outlook.com>
2019-04-17 22:52:06 -0700

    added form, edit function and test for passenger controller

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -39,6 +39,62 @@ describe PassengersController do
     # Your tests go here
   end
 
+  describe "edit" do
+    it "renders the form to edit a driver" do
+      driver = Driver.create(name: "test driver", vin: "badnvkadkvbdjoihuy79y")
+      valid_driver_id = driver.id
+      get edit_driver_path(valid_driver_id)
+      must_respond_with :success
+    end
+    it "responds with 404 when trying to edit a non existant driver" do
+      invalid_driver_id = "im a  fake id"
+      get edit_driver_path(invalid_driver_id)
+      must_respond_with :not_found
+    end
+  end
+  describe "update" do
+    it "updates an existing driver" do
+      create_input = {
+        name: "The transporter",
+        vin: "vinvinvin123",
+      }
+      driver_to_update = Driver.create(create_input)
+      update_input = {
+        "driver": {
+          name: "Not the transporter",
+          vin: "hey, what's up?",
+        },
+      }
+      expect {
+        patch driver_path(driver_to_update.id), params: update_input
+      }.wont_change "Driver.count"
+      must_respond_with :redirect
+      driver_to_update.reload
+      expect(driver_to_update.name).must_equal update_input[:driver][:name]
+      expect(driver_to_update.vin).must_equal update_input[:driver][:vin]
+    end
+    it "will return a bad_request when trying to update with invalid data" do
+      create_input = {
+        name: "The transporter",
+        vin: "vinvinvin123",
+      }
+      driver_to_update = Driver.create(create_input)
+      update_input = {
+        "driver": {
+          name: "",
+          vin: "hey, what's up?",
+        },
+      }
+      expect {
+        patch driver_path(driver_to_update.id), params: update_input
+      }.wont_change "Driver.count"
+      must_respond_with :bad_request
+      driver_to_update.reload
+      expect(driver_to_update.name).must_equal create_input[:name]
+      expect(driver_to_update.vin).must_equal create_input[:vin]
+    end
+  end
+
   describe "update" do
     # Your tests go here
   end
