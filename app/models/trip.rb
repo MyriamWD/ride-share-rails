764b6dc23eae5fbd9df7217c502385598a57b487
MyriamWD <myriam.waldend@gmail.com>
2019-04-19 00:19:59 -0700

    added validation to edit and create partial form for trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,10 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+
+  validates :date, presence: true
+  validates :rating, presence: true
+  validates :cost, presence: true
+  validates :driver_id, presence: true
+  validates :passenger_id, presence: true
 end
