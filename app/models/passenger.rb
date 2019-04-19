141d938f9b710b8b3215f2fe1c96f243510c2e52
Ngoc Le <ngocle@outlook.com>
2019-04-18 22:16:52 -0700

    added total cost method to passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,10 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_cost
+    total = self.trips.sum { |trip| trip.cost } / 100
+
+    return total
+  end
 end
